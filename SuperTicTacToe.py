e2f6b4320547e3bd0283dc9d3133e4809ec5ad28
Blakeh36 <blake.holman17@gmail.com>
2017-08-11 23:27:21 -0500

    Set up restrictions for level 2 board
    Restricts possible plays to abide by the game rules. Needs to be
    implemented to other levels. Also, this version doesn't support actually
    completing the game, as the mechanics need to be reworked to account for
    different levels.

--- SuperTicTacToe.py
+++ SuperTicTacToe.py
@@ -29,7 +29,7 @@ oneThird = display_height // 3
 
 def setPoints(lv):  # outputs locations based on their level. These are the upper right-hand corners of the squares.
     points = []
-    for n in range (1, 3 ** lv):
+    for n in range(1, 3 ** lv):
         points.append(n * display_height // 3 ** lv)
     #print(points)
     return points
@@ -80,9 +80,13 @@ def mouseInSquare(lv):
             if mousey > point[1] and mousey < point[1] + display_height // 3 ** lv:
                 return point
     return [3,3]
+def mouseInSquare2(lv, x, y):
+    for point in allTopLeftPoints(lv)[0]:
+        if x > point[0] and x < point[0] + display_height // 3 ** lv:
+            if y > point[1] and y < point[1] + display_height // 3 ** lv:
+                return point
 
 
-locations = [[oneThird, oneThird], [twoThird, 0], [oneThird, 0], [0, 0], [twoThird, oneThird],[0,oneThird], [twoThird, twoThird], [oneThird, twoThird], [oneThird, twoThird], [0, twoThird] ]
 def drawx(list, lv):
     if list != [3,3]:
         x = list[0]
@@ -123,46 +127,79 @@ def threeRow(record):
             counter2 += 1
             if counter2 == 3:
                 win = True
-
-    #print('coords')
-    #print(record)
     return win
-
-
-
 
 turn = 0
 xRecords = []
 circleRecords = []
+allRecords = []
 stop = False
 while not stop:
 
     for event in pygame.event.get():
 
-
         if event.type == pygame.MOUSEMOTION:
             mousex, mousey = event.pos
+
         elif event.type == pygame.MOUSEBUTTONDOWN:
 
-            if turn % 2 == 0 and mouseInSquare(level) not in circleRecords and mouseInSquare(level) not in xRecords: #x's turn
-                drawx(mouseInSquare(level), level)
-                turn += 1
-                xRecords.append(mouseInSquare(level))
+            if len(xRecords) == 0:
 
-            elif turn % 2 != 0 and mouseInSquare(level) not in xRecords and mouseInSquare(level) not in circleRecords: #circle's turn
-                drawCircle(mouseInSquare(level), level)
-                turn += 1
-                circleRecords.append(mouseInSquare(level))
+                if turn % 2 == 0 and mouseInSquare(level) not in circleRecords and mouseInSquare(level) not in xRecords: #x's turn
+                    drawx(mouseInSquare(level), level)
+                    turn += 1
+                    xRecords.append(mouseInSquare(level))
+                    allRecords.append(mouseInSquare(level))
 
-            if threeRow(xRecords) == True: #x wins
-                gameDisplay.fill(white)
+                elif turn % 2 != 0 and mouseInSquare(level) not in xRecords and mouseInSquare(level) not in circleRecords: #circle's turn
+                    drawCircle(mouseInSquare(level), level)
+                    turn += 1
+                    circleRecords.append(mouseInSquare(level))
+                    allRecords.append(mouseInSquare(level))
 
-            elif threeRow(circleRecords) == True: #circle wins
-                gameDisplay.fill(green)
+                if threeRow(xRecords) == True: #x wins
+                    gameDisplay.fill(white)
 
-            elif len(xRecords) + len(circleRecords) == 9: #tie
-                gameDisplay.fill(black)
+                elif threeRow(circleRecords) == True: #circle wins
+                    gameDisplay.fill(green)
 
+                elif len(xRecords) + len(circleRecords) == 9: #tie
+                    gameDisplay.fill(black)
+            else:
+                req = mouseInSquare2(level, allRecords[(turn - 1)][0] + 1, allRecords[(turn - 1)][1] + 1)
+                req2 = mouseInSquare2(level - 1, allRecords[(turn - 1)][0] + 1, allRecords[(turn - 1)][1] + 1)
+
+                xReqLower = (req[0] - req2[0]) * 3 ** (level - 1)
+                yReqLower = (req[1] - req2[1]) * 3 ** (level - 1)
+
+                xReqUpper = xReqLower + display_height // 3 ** (level - 1)
+                yReqUpper = yReqLower + display_height // 3 ** (level - 1)
+
+                #print("between" + str(xReq) + " and " + str(xReq + display_height // 3 ** (level - 1)))
+                #print("between" + str(yReq) + " and " + str(yReq + display_height // 3 ** (level - 1)))
+
+                if xReqLower < mousex and mousex < xReqUpper and yReqLower < mousey and mousey < yReqUpper:
+
+                    if turn % 2 == 0 and mouseInSquare(level) not in circleRecords and mouseInSquare(level) not in xRecords:  # x's turn
+                        drawx(mouseInSquare(level), level)
+                        turn += 1
+                        xRecords.append(mouseInSquare(level))
+                        allRecords.append(mouseInSquare(level))
+
+                    elif turn % 2 != 0 and mouseInSquare(level) not in xRecords and mouseInSquare(level) not in circleRecords:  # circle's turn
+                        drawCircle(mouseInSquare(level), level)
+                        turn += 1
+                        circleRecords.append(mouseInSquare(level))
+                        allRecords.append(mouseInSquare(level))
+
+                    if threeRow(xRecords) == True:  # x wins
+                        gameDisplay.fill(blue)
+
+                    elif threeRow(circleRecords) == True:  # circle wins
+                        gameDisplay.fill(red)
+
+                    elif len(xRecords) + len(circleRecords) == 1000:  # tie
+                        gameDisplay.fill(black)
         #drawBoard(oneThird, oneThird, oneThird, green)
         drawBoard(level)
 
